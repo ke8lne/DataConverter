a9218f06d44e44aac2f0091fd2272f5471ce7e74
Ke8Ine <zyxgwaps123@gmail.com>
2023-06-16 02:05:20 +0800

    Adjusted scrollbar width.

--- src/App.java
+++ src/App.java
@@ -333,6 +333,7 @@ public class App extends JFrame {
     }
 
     public static void main(String... args) {
+        UIManager.put("ScrollBar.width", 25);
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 new App();
